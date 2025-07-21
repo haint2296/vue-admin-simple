32f16ab6ff1fb5fcc96cebe4ea2389c091f6e0a0
Hai Nguyen <haint2296@gmail.com>
2025-07-22 01:49:05 +0700

    feat: add common component primevue

---
+++ src/shared/ui/input/input.vue
@@ -0,0 +1,37 @@
+<template>
+  <input
+    v-bind="$attrs"
+    v-model="model"
+    :placeholder="props.placeholder"
+    :type="props.type"
+    :name="props.name"
+    :class="
+      cn(
+        'transition-all duration-200',
+        'text-base shadow-xs md:text-sm',
+        'selection:bg-primary bg-transparent',
+        'px-3 py-1',
+        'rounded-md border border-[var(--input)]',
+        'outline-none focus-visible:ring-[2px] focus-visible:ring-[var(--primary)]',
+        'h-9 w-full max-w-xs min-w-0',
+        props.disabled && 'cursor-not-allowed opacity-50',
+      )
+    "
+    :autocomplete="props.autocomplete"
+    :disabled="props.disabled"
+  />
+</template>
+
+<script setup lang="ts">
+import { cn } from '@/shared/lib/utils'
+import { type InputProps } from './input.types'
+
+const model = defineModel<string>({ required: true })
+
+const props = withDefaults(defineProps<InputProps>(), {
+  placeholder: '',
+  type: 'text',
+  autocomplete: 'off',
+  disabled: false,
+})
+</script>
