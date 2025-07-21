0a1b3d7c7caeb2d640871f3b427e85ba39d1f0d1
Hai Nguyen <hainguyen@admins-Mac-mini.local>
2025-07-21 18:01:59 +0700

    Updates

--- src/shared/services/api/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './hooks/products/products'
-export * from './hooks/users/users'
-export * from './models'
--- src/shared/services/msw/handlers.ts
+++
@@ -1,4 +0,0 @@
-import { getProductsMock } from '../api/hooks/products/products.msw'
-import { getUsersMock } from '../api/hooks/users/users.msw'
-
-export const handlers = [...getProductsMock(), ...getUsersMock()]
