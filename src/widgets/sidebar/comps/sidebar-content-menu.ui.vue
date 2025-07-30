434707e23c9d9f0693e3a663e741a2178d329fd8
Hai Nguyen <haint2296@gmail.com>
2025-07-31 02:16:06 +0700

    feat: add sidebar

---
+++ src/widgets/sidebar/comps/sidebar-content-menu.ui.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <ul class="flex min-w-0 flex-col gap-1">
+    <slot />
+  </ul>
+</template>
--- src/widgets/sidebar/types/index.ts
+++ src/widgets/sidebar/types/index.ts
@@ -1,5 +1,13 @@
+import type { Component } from 'vue'
+
 type SidebarProviderProps = {
   modelValue: boolean
 }
 
-export type { SidebarProviderProps }
+type SidebarContentMenuItemProps = {
+  title: string
+  icon?: Component
+  subItems?: Array<SidebarContentMenuItemProps> | undefined
+}
+
+export type { SidebarContentMenuItemProps, SidebarProviderProps }
